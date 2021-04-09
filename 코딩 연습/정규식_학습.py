b26981763376a1d272195e111862b17a54ab121b
홍원종 <wonhong1996@naver.com>
2021-04-09 15:10:09 +0900

    vs

---
+++ 코딩 연습/정규식_학습.py
@@ -0,0 +1,20 @@
+import re
+import requests
+import numpy as np
+from bs4 import BeautifulSoup
+text = '010-1566#7152'
+
+parse = re.sub('[-=.#?:$]', '', text)
+
+
+# 이제 데이터베이스 파일을 가져와서 원하는 문자열을 제거해보자.
+
+with open("database.txt", "r", encoding = "UTF-8") as f:
+    database = f.read()
+
+cleaned_database = re.sub('[<>]', '', database)
+
+print(cleaned_database)
+
+with open("c_database.txt", "w", encoding = 'UTF-8') as f:
+    f.write(cleaned_database)
