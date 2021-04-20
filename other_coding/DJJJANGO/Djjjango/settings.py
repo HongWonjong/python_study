a3405098c14cb603a431409e2fd914b9901369e9
홍원종 <wonhong1996@naver.com>
2021-04-20 18:06:49 +0900

    20210420

--- other_coding/DJJJANGO/Djjjango/settings.py
+++ other_coding/DJJJANGO/Djjjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Djjjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'db1',
+        'USER': 'postgresql', # 유저 이름은 postgresql로 써야 한다.
+        'PASSWORD': 'retopia-486',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-KR'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'KST'
 
 USE_I18N = True
 
