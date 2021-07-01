cec51cc02eefcbf240880ded198771dee3699941
wonjong Hong <wonhong1996@naver.com>
2021-07-02 02:14:56 +0900

    20210702

--- other_coding/Flask/run1.py
+++ other_coding/Flask/run1.py
@@ -8,6 +8,7 @@ from flask import abort
 from flask import redirect
 from flask import url_for
 import time
+import math
 
 
 app = Flask(__name__)
@@ -20,25 +21,78 @@ def format_datetime(value):
     if value is None:
         return ""
     now_timestamp = time.time()
-    datetime.fromtimestamp(now_timestamp) 
-    datetime.utcfromtimestamp(now_timestamp) 
-    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp) 
-    value = datetime.fromtimestamp(int(value) / 1000) + offset 
+    datetime.fromtimestamp(now_timestamp)
+    datetime.utcfromtimestamp(now_timestamp)
+    offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
+    value = datetime.fromtimestamp(int(value) / 1000) + offset
     return value.strftime("%Y-%m-%d %H:%M:%S")
 
 
 @app.route("/list")
 def lists():
+    # 페이지 값(값이 없는 경우 기본 값은 1)
     page = request.args.get("page", default=1, type=int)
+    # 한 페이지 당 몇 개의 게시물을 출력할지 정해야 함.
+    limit = request.args.get("limit", 10, type=int)
+
+    search = request.args.get("search", -1, type=int)
+    keyword = request.args.get("keyword", type=str)
+
+    # 최종적으로 완성된 쿼리를 만들 변수
+    query = {}
+    # 검색어 상태를 추가할 리스트 변수
+    search_list = []
+
+    if search == 0:
+        search_list.append({"title": {"$regex": keyword}})
+    elif search == 1:
+        search_list.append({"contents": {"$regex": keyword}})
+    elif search == 2:
+        search_list.append({"title": {"$regex": keyword}})
+        search_list.append({"contents": {"$regex": keyword}})
+    elif search == 3:
+        search_list.append({"name": {"$regex": keyword}})
+
+    # 검색 대상이 한 개라도 존재할 경우 query 변수에 $or 리스트를 쿼리합니다.
+    if len(search_list) > 0:
+        query = {"$or": search_list}
+
+    print(query)
     board = mongo.db.board
-    datas = board.find({})
-    return render_template("list.html", datas=datas)
+    datas = board.find(query).skip((page-1) * limit).limit(limit)
+    tot_count = board.find(query).count()
+    # 게시물의 총 개수를 구할 수 있음
+    # 마지막 페이지의 수를 구함. 올림을 해 주어야 함.
+    last_page_num = math.ceil(tot_count / limit)
+    # 페이지 블럭은 5개씩 표기.
+    block_size = 5
+    # 현재 블럭의 위치를 구해야 함.
+    block_num = int((page - 1) / block_size)
+    # 블럭의 시작 위치를 구해주어야 함.
+    block_start = int((block_size * block_num) + 1)
+    # 블럭의 끝 위치
+    block_last = math.ceil(block_start + (block_size - 1))
+
+
+    return render_template(
+            "list.html",
+            datas=datas,
+            limit=limit,
+            page=page,
+            block_start=block_start,
+            block_last=block_last,
+            last_page_num=last_page_num,
+            search=search,
+            keyword=keyword)
 
 
 @app.route("/view/<idx>")
 def board_view(idx):
     # idx = request.args.get("idx")
     if idx is not None:
+        page = request.args.get("page")
+        search = request.args.get("search")
+        keyword = request.args.get("keyword")
         board = mongo.db.board
         data = board.find_one({"_id": ObjectId(idx)})
 
@@ -52,7 +106,12 @@ def board_view(idx):
                 "view": data.get("view")
             }
 
-            return render_template("view.html", result=result)
+            return render_template(
+                "view.html",
+                result=result,  
+                page=page,
+                search=search,
+                keyword=keyword)
     return abort(404)
 
 
