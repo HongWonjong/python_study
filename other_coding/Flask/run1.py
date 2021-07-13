8948ceb03db3610c72b69a21081b3d8d1b92b840
홍원종 <wonhong1996@naver.com>
2021-07-13 13:13:39 +0900

    0713

--- other_coding/Flask/run1.py
+++ other_coding/Flask/run1.py
@@ -1,27 +1,3 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask_pymongo import PyMongo
-from datetime import datetime
-from datetime import timedelta
-from bson.objectid import ObjectId
-from flask import abort
-from flask import redirect
-from flask import url_for
-from flask import flash
-from flask import session
-from functools import wraps
-import time
-import math
-
-
-app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myweb"
-app.secret_key = "ABCD"
-app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=30)
-mongo = PyMongo(app)
-
-
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
