e7b72eafc9f925c1e6bc034dab14f508be62f8be
홍원종 <wonhong1996@naver.com>
2021-09-15 18:38:17 +0900

    2021_09_15

--- other_coding/boiler-plate/index.js
+++ other_coding/boiler-plate/index.js
@@ -14,6 +14,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/register', (req, res) => {
+   // 회원가입 할 때 필요한 정보들을 client에서 가져오면, 
+   // 그것들을 데이터 베이스에 넣어주는 기능이 포함된 route이다.
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
